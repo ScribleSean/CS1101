da1816481cd4db9e58e298dc567bf3057eb56952
scriblesean <sean.arackal@gmail.com>
2022-10-05 15:07:18 -0400

    initial-commit
    question 4

--- kratman-a-arackal-s-hw6.rkt
+++ kratman-a-arackal-s-hw6.rkt
@@ -5,21 +5,22 @@
 ;; WPI Usernames: aekratman & sarackal
 ;; Homework #6
 
+
 ;; Question 1
 
 
 (define-struct message (username text read?))
 ;; Message is a (make-message String String Boolean)
 ;; interp: represents a message.
-;; username is a String
-;; text is a String
-;; read? is a Boolean
+;; username is a String that represents message sender's username
+;; text is a String that represents the text of the message
+;; read? is a Boolean that represents whether or not the user has read the message
 
 (define-struct user (username mailbox))
 ;; User is a (make-user String ListOfMessage)
 ;; interp: represents a user.
-;; username is a String
-;; mailbox is a ListOfMessage
+;; username is a String that represents a user's username
+;; mailbox is a ListOfMessage that represents 
 
 
 ;; ListOfMessage is one of
@@ -30,24 +31,48 @@
 
 ;; Question 2
 
-(define Mailsys empty)
-(define Newuser (make-user "Newuser" empty))
+
+(define mailsys empty) ;;empty email system with no users
+(define newuser (make-user "Newuser" empty)) ;;user with name "Newuser" and an empty list of messages
+
 
 ;; Question 3
 
+;; helper-new-user: ListOfString --> ListOfString
+;; helper function for add-new-user that conses the new given username to mailsys
+
+(define (helper-new-user username)
+  (cons (make-user username empty) mailsys))
+
+;; add-new-user: String --> (void)
+;; adds a new user with given username 
 (define (add-new-user username)
-  (set! Mailsys (cons (make-user username empty) Mailsys)))
+  (set! mailsys (helper-new-user username)))
 ;; EFFECT: May increase the length of the Mail system.
 
+(add-new-user "Sean")
 (add-new-user "Abbey")
+
 
 ;; Question 4
 
-(define (send-email-message sendername reipientname text)
-  (set! user 
 
-;Write a function send-email-message that consumes 
-;the name of the sender of an email, the name of the recipient of the email, and the 
-;text of an email message, and produces void. The effect of the function is to store 
-;a new unread message in the recipient's mailbox. Assume the named recipient is a 
-;user in the mail system.
+;;helper-find-user: String ListOfUsername --> Username
+;;helper function for helper-send-email to find a username in a given ListOfUsername
+(define (helper-find-user recipient-name alou)
+ (cond [(empty? alou) (error "username not found")]
+       [(cons? alou) (if (string=? recipient-name (user-username (first alou)))
+                         (first alou)
+                         (helper-find-user recipient-name (rest alou)))]))
+
+;;helper-send-email: String String String ListOfUsername --> (void)
+;;helper function for send-email-message
+(define (helper-send-email sender-name recipient-name email alou)
+  (set-user-mailbox! (helper-find-user recipient-name alou)
+                     (cons (make-message (helper-find-user recipient-name alou) email true)
+                     (user-mailbox (helper-find-user recipient-name alou)))))
+
+;; send-email-message: String String String --> (void)
+;; to store a new unread message in the recipient's mailbox.
+(define (send-email-message sender-name recipient-name email)
+  (helper-send-email sender-name recipient-name email mailsys))
